0ed4b89061f3db2ad91da92c57574c4c9654d839
olivertenhoor <oliver@tenhoor.org>
2024-08-02 00:14:25 +0200

    test commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # rouletteApp
+first run "npm install" in your terminal when in the right dir.
+then run with: node app.js
+
+dependencies:
+    sudo apt install nodejs
+    npm install
+    npm install mysql2
--- app.js
+++ app.js
@@ -6,9 +6,18 @@ const port = process.env.PORT || 3000;
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+
+
+
+app.get('/home', (req, res) => {
+    res.render(path.join(__dirname, 'public', 'home.html'));
 });
+
+app.get('/RouletteWheel', (req, res) => {
+    res.render(path.join(__dirname, 'public', 'index.html'));
+});
+
+
 
 
 
