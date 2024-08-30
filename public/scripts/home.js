e5a4c225d269d147113a2601419d03364c9f0256
olivertenhoor <oliver@tenhoor.org>
2024-08-30 17:03:11 +0200

    bugfix

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -12,10 +12,16 @@ fetch('navbar.html')
 
     
 fetch('footer.html')
-.then(response => response.text())
-.then(data => {
-    document.getElementById('footer-container').innerHTML = data;
-});
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('footer-container').innerHTML = data;
+                const scriptTags = document.querySelectorAll('#navbar-container script');
+                scriptTags.forEach(scriptTag => {
+                    const newScript = document.createElement('script');
+                    newScript.textContent = scriptTag.textContent;
+                    document.body.appendChild(newScript);
+                });
+            }   );
 
 
 
