73052511c3119a300c73145943283814bb23e974
olivertenhoor <oliver@tenhoor.org>
2024-08-30 16:56:26 +0200

    about us

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,11 +1,21 @@
-document.getElementById('wiel').addEventListener('click', function() {
-    window.location.href = 'roulettewheel.html'; // Redirect to a different page
+fetch('navbar.html')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('navbar-container').innerHTML = data;
+                const scriptTags = document.querySelectorAll('#navbar-container script');
+                scriptTags.forEach(scriptTag => {
+                    const newScript = document.createElement('script');
+                    newScript.textContent = scriptTag.textContent;
+                    document.body.appendChild(newScript);
+                });
+            });
+
+    
+fetch('footer.html')
+.then(response => response.text())
+.then(data => {
+    document.getElementById('footer-container').innerHTML = data;
 });
 
-document.getElementById('loginButton').addEventListener('click', function() {
-    window.location.href = 'login.html'; // Redirect to a different page
-});
 
-document.getElementById('createAccount').addEventListener('click', function() {
-    window.location.href = 'createAccount.html'; // Redirect to a different page
-});
+
